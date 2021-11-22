49f9334d8e13d74f840bc8ec845741fff8732b59
Tamunotonye Adokiye Emmanuel <57795974+ksleak@users.noreply.github.com>
2021-11-22 18:32:04 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import '../styles/index.css';
 
+import React from "react";
+import ReactDOM from "react-dom";
+
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
 }
